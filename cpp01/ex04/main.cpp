2f91cfff0c9f2e6b38a175acc92403da1a02e3ca
Ashot <ashotd03@gmail.com>
2023-09-26 12:31:27 +0400

    case with empty to_look field fixed

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -9,13 +9,14 @@ std::string	replace(std::string text, std::string to_find, std::string replace)
 	std::string	tmp;
 
 	new_pos = text.find(to_find, 0);
-	if (new_pos == std::string::npos){
+	if (new_pos == std::string::npos || to_find.empty()){
 		tmp = text;
 		return (tmp);
 	}
 	tmp = text.substr(0, new_pos);
 	tmp = tmp + replace;
 	while (text.find(to_find, new_pos + to_find.size()) != std::string::npos){
+		std::cout << "here";
 		old_pos = new_pos;
 		new_pos = text.find(to_find, new_pos + to_find.size());
 		tmp = tmp + text.substr(old_pos + to_find.size(), new_pos - (old_pos + to_find.size()));
