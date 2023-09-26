594ef62c1d06d6681a7d2aa8d5e57d46e4ae1b06
Ashot <ashotd03@gmail.com>
2023-09-26 17:01:11 +0400

    ex05 done, probably

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,12 @@
+#include <string>
+
+class Harl {
+	void	debug(void);
+	void	info(void);
+	void	warning(void);
+	void	error(void);
+public:
+	Harl();
+	~Harl();
+	void	complain (std::string level);
+};
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,10 @@
+#include "Harl.hpp"
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::string comp = "";
+	Harl test;
+	test.complain(comp);
+}
