7c337d006f71ce14e0b1dfe1b3e6b332d4a39175
Ashot <ashotd03@gmail.com>
2023-10-23 20:41:02 +0400

    cure

---
+++ cpp04/ex03/Cure.cpp
@@ -0,0 +1,26 @@
+#include "Cure.hpp"
+
+Cure::Cure(){
+	std::cout << "Cure default constructor called" << std::endl;
+	this->type = "Cure";
+}
+
+Cure::~Cure(){
+	std::cout << "Cure Destructor called" << std::endl;
+}
+
+Cure::Cure(const Cure &ref){
+	std::cout << "Cure copy constructor called" << std::endl;
+	type = ref.type;
+}
+
+Cure & Cure::operator=(const Cure &ref){
+	std::cout << "Cure '=' operator overload" << std::endl;
+	if (this == &ref)
+	{
+		std::cout << "Same Cure" << std::endl;
+		return (*this);
+	}
+	type = ref.type;
+	return (*this);
+}
