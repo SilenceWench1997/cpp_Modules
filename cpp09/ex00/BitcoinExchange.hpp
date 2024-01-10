7cce7d084f6faeef2ef206c0135f615cbf7901e7
Ashot <ashotd03@gmail.com>
2024-01-10 21:31:52 +0400

    parser done

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -3,16 +3,25 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 class Btc{
 private:
 	std::string line;
 	std::string date;
 	std::string value;
+	std::string year;
+	std::string month;
+	std::string day;
 	size_t pipePos;
+	int monthNum;
+	int dayNum;
+	int yearNum;
+	void splitData();
 	void checkline();
 	void parseDate();
 	void parseValue();
+	bool checkMonthDay();
 public:
 	void parse(std::string filename);
 };
