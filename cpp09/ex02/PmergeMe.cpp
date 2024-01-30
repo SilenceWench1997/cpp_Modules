fc87b6f6bc111759877e00e096e7383a894402fb
Ashot <ashotd03@gmail.com>
2024-01-30 17:15:45 +0400

    working on it

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,22 +1,66 @@
 #include "PmergeMe.hpp"
 
-std::vector<int> PM::sortV(){
-	std::vector<std::pair<int, int>> pair;
+std::vector<int> &PM::getArrV(){
+	return (arrV);
+}
+
+std::vector<std::pair<int, int> > PM::createPairs(const std::vector<int> &arrVt){
+	std::vector<std::pair<int, int> > pair;
 	std::vector<int> largeElems;
-	if (arrV.size() % 2 == 1){
-		//Odd argument
-	}
-	for (size_t i = 0; i < arrV.size(); i += 2){
-		if (i + 1 < arrV.size()){
-			pair.push_back(std::pair<int, int>(arrV[i], arrV[i + 1]));
-		}
-	}
-	for (size_t i = 0; i < pair.size(); i++){
-		(pair[i].first > pair[i].second) ? 
-		largeElems.push_back(pair[i].first):
-		largeElems.push_back(pair[i].first);
-	}
 	
+	for (size_t i = 0; i < arrVt.size(); i += 2){ //creating pairs
+		if (i + 1 < arrVt.size())
+			pair.push_back(std::pair<int, int>(arrVt[i], arrVt[i + 1]));
+		else
+			pair.push_back(std::pair<int, int>(arrVt[i], -1));
+	}
+	return (pair);
+}
+
+void PM::merge(std::vector<int> &elems, size_t start, size_t middle, size_t end){
+	size_t l = middle - start + 1;
+	size_t r = end - middle;
+
+	std::vector<int> left, right;
+	for(size_t i = 0; i < l; i++)
+		left[i] = elems[start + i];
+	for(size_t i = 0; i < r; i++)
+		right[i] = elems[middle + 1 + i];
+	
+	int i = 0, j = 0, k = start;
+	while (i < l && j < r){
+		if (left[i] <= right[j]) elems[k] = left[i++];
+		else elems[k] = right[j++];
+		k++;
+	}
+}
+
+void PM::mergeSort(std::vector<int> &elems, size_t start, size_t end){
+	if (start < end){
+		size_t middle = start + (end - start) / 2;
+		mergeSort(elems, start, middle);
+		mergeSort(elems, middle + 1, end);
+
+		merge(elems, start, middle, end);
+	}
+}
+
+std::vector<int> PM::sortV(const std::vector<int> &arrVt){
+	std::vector<int> largeElems;
+	std::vector<std::pair<int, int> > pair;
+	size_t i = 0;
+	std::vector<int> sortedArr;
+
+	if (arrVt.size() == 1){
+		return (arrVt);
+	}
+	pair = createPairs(arrVt);
+	while(i < pair.size()){ //choosing the bigger elements from each pair
+		largeElems.push_back(std::max(pair[i].first, pair[i].second));
+		i++;
+	}
+	mergeSort(largeElems, 0, largeElems.size() - 1);
+	return (sortedArr);
 }
 
 void PM::parse(std::string input){
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -4,13 +4,18 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 class PM{
 private:
 	std::vector<int> arrV;
 public:
 	void parse(std::string input);
-	void sortV();
-};
+	std::vector<int> &getArrV();
+	std::vector<std::pair<int, int> > createPairs(const std::vector<int> &arrVt);
+	std::vector<int> sortV(const std::vector<int> &arrVt);
+	void mergeSort(std::vector<int> &elems, size_t start, size_t end);
+	void merge(std::vector<int> &elems, size_t start, size_t middle, size_t end);
 
+};
 #endif
